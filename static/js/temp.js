bcb4e83a5546593f3815ef50f341dc7fa5e9eea5
3d-Nation <55363526+3d-Nation@users.noreply.github.com>
2025-09-28 16:02:54 -0500

    cut HTML docs and put into .tmpl docs

---
+++ static/js/temp.js
@@ -0,0 +1,29 @@
+ /*
+ document.getElementById("colorButton").addEventListener("click", function(){
+        
+        let html = `
+        <form id="check-availability-form" action="" method="post" novalidate call="needs-validatiaon">
+          <div class="row">  
+            <div class="col">
+              <div class="row" id="reservation-dates-modal">
+                <div class="col">
+                  <input disabled required class="form-control" type="text" name="start" id="start" placeholder="Arrival">
+                </div>
+                <div class="col">
+                  <input disabled required class="form-control" type="text" name="end" id="end" placeholder="Departure">
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+        `
+        attention.custom({msg: html, title: "Choose your dates"})
+
+      })
+*/
+
+ //removed from base.llayout.tmpl...above notify...
+      // const elem = document.getElementById('reservation-dates');
+      // const rangepicker = new DateRangePicker(elem, {
+      //   format: "yyyy-mm-dd",
+      // });
